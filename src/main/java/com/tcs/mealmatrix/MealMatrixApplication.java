254eb68705e9d35600d8ac571617e69cf73a5ed2
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2026-05-23 16:32:09 +0530

    Initial commit of Spring project

---
+++ src/main/java/com/tcs/mealmatrix/MealMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.mealmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMatrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealMatrixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tcs/mealmatrix/MealMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.mealmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMatrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
